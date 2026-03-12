5488ccf23eb5b5a48d2afd4e92bac30bcde6ce23
viethq <viethq.1906@gmail.com>
2026-03-13 00:23:17 +0700

    feat: official queries for tpch

---
+++ src/experiment/tpch/queries/q16_s555.sql
@@ -0,0 +1,30 @@
+select
+	p_brand,
+	p_type,
+	p_size,
+	count(distinct ps_suppkey) as supplier_cnt
+from
+	partsupp,
+	part
+where
+	p_partkey = ps_partkey
+	and p_brand <> 'Brand#32'
+	and p_type not like 'ECONOMY POLISHED%'
+	and p_size in (5, 36, 10, 45, 23, 34, 26, 9)
+	and ps_suppkey not in (
+		select
+			s_suppkey
+		from
+			supplier
+		where
+			s_comment like '%Customer%Complaints%'
+	)
+group by
+	p_brand,
+	p_type,
+	p_size
+order by
+	supplier_cnt desc,
+	p_brand,
+	p_type,
+	p_size;
---
+++ src/experiment/tpch/queries/q22_s456.sql
@@ -0,0 +1,37 @@
+select
+	cntrycode,
+	count(*) as numcust,
+	sum(c_acctbal) as totacctbal
+from
+	(
+		select
+			substring(c_phone from 1 for 2) as cntrycode,
+			c_acctbal
+		from
+			customer
+		where
+			substring(c_phone from 1 for 2) in
+				('34', '27', '41', '29', '43', '28', '39')
+			and c_acctbal > (
+				select
+					avg(c_acctbal)
+				from
+					customer
+				where
+					c_acctbal > 0.00
+					and substring(c_phone from 1 for 2) in
+						('34', '27', '41', '29', '43', '28', '39')
+			)
+			and not exists (
+				select
+					*
+				from
+					orders
+				where
+					o_custkey = c_custkey
+			)
+	) as custsale
+group by
+	cntrycode
+order by
+	cntrycode;
